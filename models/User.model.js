861b19891f1cca7355e4430a03545e68fa1a4494
l-goede <luise.goede@yahoo.de>
2021-12-09 12:32:41 +0100

    create Usermodel

--- models/User.model.js
+++ models/User.model.js
@@ -1,20 +1,20 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: String,
+// 1. Define your schema
+let UserSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
   },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
-);
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+});
 
-const User = model("User", userSchema);
+// 2. Define your model
+let User = model("user", UserSchema);
 
+// 3. Export your Model with 'module.exports'
 module.exports = User;
