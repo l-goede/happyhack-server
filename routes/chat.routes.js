cf4815d7ebdd7ae3d5d68b118911cdabf8f9b8df
m4rcosD <mp.dias023@gmail.com>
2021-12-14 23:00:10 +0000

    5.5

--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -2,14 +2,20 @@ const router = require("express").Router();
 const Conversation = require('../models/Conversation.model')
 const Message = require('../models/Message.model')
 
+// A route to return the converstaion id between two participants if it already exists
+// or create a new converstaion, when users chat for the first time
 router.post('/conversation', (req, res, next) => {
+    //The user will send an array of participant ids in the chat (usually just two)
+    // eg. participants = ['609b63324f3c1632c8ff35f4', '609b63644f3c1632c8ff35f5']
     const {participants} = req.body
     Conversation.findOne({ participants: { $all: participants} })
       .then((found) => {
         if (found) {
+          //Conversation between those participants already present
           res.status(200).json(found)
         }
         else {
+          //Create a conversation between them if not present
           Conversation.create({participants})
             .then((response) => {
               res.status(200).json(response)
@@ -21,6 +27,7 @@ router.post('/conversation', (req, res, next) => {
       })
 })
 
+// A route to get all messages of a certain converstaion 
 router.get('/messages/:conversationId', (req, res, next) => {
   const {conversationId} = req.params
   Message.find({conversationId})
