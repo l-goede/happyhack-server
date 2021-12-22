876613a46a7e6714e7c94149ed6190f3d93b8ea8
m4rcosD <mp.dias023@gmail.com>
2021-12-22 17:55:59 +0000

    hey 2

--- routes/seeds/event.seed.js
+++ routes/seeds/event.seed.js
@@ -6,29 +6,30 @@ let image =
   "https://i.pinimg.com/originals/97/35/2a/97352a18b151610dc66f3f693e0b9196.png";
 let events = [
   {
-    image,
-    title: "Coding Convention",
-    location: "Heaven",
-    date: new Date("2021-12-24"),
+    image: "https://www.weareamsterdam.com/wp-content/uploads/2018/07/IMG_1315-e1551362907538.jpg",
+    title: "Code Party at IronHack",
+    location: "Amsterdam",
+    date: new Date("2022-01-15"),
     description:
-      "Excited to inform you, we having the last coding convention this year. Join the event, its free!",
+      "A nice display flex party with Manish, if you got lucky maybe can get a fancy black hoodie",
   },
   {
-    image,
-    title: "Coding Convention",
-    location: "Heaven",
-    date: new Date("2021-12-24"),
+    image: "https://res.cloudinary.com/teepublic/image/private/s--6vRODuCH--/c_crop,x_10,y_10/c_fit,h_830/c_crop,g_north_west,h_1038,w_1038,x_-295,y_-104/l_upload:v1565806151:production:blanks:vdbwo35fw6qtflw9kezw/fl_layer_apply,g_north_west,x_-406,y_-215/b_rgb:fffffe/c_limit,f_jpg,h_630,q_90,w_630/v1567010453/production/designs/5769420_0.jpg",
+    title: "Coffee and code",
+    location: "Porto",
+    date: new Date("2022-01-19"),
     description:
-      "Excited to inform you, we having the last coding convention this year. Join the event, its free!",
+      "An awesome code reunion with Rodrigao, Joshua to code some cool Surf app",
   },
   {
-    image,
-    title: "Coding Convention",
-    location: "Heaven",
-    date: new Date("2021-12-24"),
+    image: "https://4.bp.blogspot.com/-UYJjvmDemn0/WT3ov0R6cpI/AAAAAAAAvgE/frFGuJ4p4Ac7umWkdZZF5qTbnKdReWP2wCLcB/s1600/landscape-1456483171-pokemon2.jpg,",
+    title: "Crazy Pokemon API party",
+    location: "India",
+    date: new Date("2022-01-27"),
     description:
-      "Excited to inform you, we having the last coding convention this year. Join the event, its free!",
+      "A cool code party with the best teatcher ever, the greatest Manish",
   },
+  
 ];
 
 EventModel.create(events)
